ebc9da1c8de29cd993c643a30757755edc284e35
Kinga Kaczmarzyk <K.K.Kaczmarzyk@student.tudelft.nl>
2024-12-15 07:42:22 +0100

    my function to recognize is not working yet

--- Recognize.py
+++ Recognize.py
@@ -6,124 +6,178 @@ from matplotlib import pyplot as plt
 
 
 def segment_and_recognize(plate_images):
-	"""
-	In this file, you will define your own segment_and_recognize function.
-	To do:
-		1. Segment the plates character by character
-		2. Compute the distances between character images and reference character images(in the folder of 'SameSizeLetters' and 'SameSizeNumbers')
-		3. Recognize the character by comparing the distances
-	Inputs:(One)
-		1. plate_imgs: cropped plate images by Localization.plate_detection function
-		type: list, each element in 'plate_imgs' is the cropped image(Numpy array)
-	Outputs:(One)
-		1. recognized_plates: recognized plate characters
-		type: list, each element in recognized_plates is a list of string(Hints: the element may be None type)
-	Hints:
-		You may need to define other functions.
-	"""
-	#1
-	#k1 parameter is percentage of max size of a letter/digit from the image/k2 for the smallest
-	#ratioStandard - percentage of the image that is a letter/digit
-	ratioStandard = 0.3
-	processEachDL(plate_images,0.1,0.2,0.01,ratioStandard)
+    """
+    In this file, you will define your own segment_and_recognize function.
+    To do:
+        1. Segment the plates character by character
+        2. Compute the distances between character images and reference character images(in the folder of 'SameSizeLetters' and 'SameSizeNumbers')
+        3. Recognize the character by comparing the distances
+    Inputs:(One)
+        1. plate_imgs: cropped plate images by Localization.plate_detection function
+        type: list, each element in 'plate_imgs' is the cropped image(Numpy array)
+    Outputs:(One)
+        1. recognized_plates: recognized plate characters
+        type: list, each element in recognized_plates is a list of string(Hints: the element may be None type)
+    Hints:
+        You may need to define other functions.
+    """
+    #1
+    #k1 parameter is percentage of max size of a letter/digit from the image/k2 for the smallest
+    #ratioStandard - percentage of the image that is a letter/digit
+    ratioStandard = 0.3
+    listOfChars = processEachDL(plate_images,0.1,0.2,0.01,ratioStandard)
 
-	#visualization
-	# plt.imshow(cv2.cvtColor(imageInverted,cv2.COLOR_GRAY2RGB))
-	# plt.title('Green')
-	# plt.axis('off')
-	# plt.show()
+    #2
+    #First will try KNN and then different algos
+    #we iterate thorugh letters then digits and calculate an error for that
+    # using switch we add a letter
+    #return a string
+    # for c in listOfChars:
+    for i in listOfChars:
+        charFound, distance = bestDistance(i)
+        plotImage(i,"original char")
+        plotImage(charFound, "charfound")
 
-	# recognized_plates = [None, None, None]
-	# return recognized_plates
+
+    # recognized_plates = [None, None, None]
+    # return recognized_plates
+
+def plotImage(img, title, cmapType=None):
+
+    if (cmapType):
+        plt.imshow(img, cmap=cmapType, vmin=0, vmax=255)
+    else:
+        plt.imshow(img, vmin=0, vmax=255)
+    plt.title(title)
+    plt.show()
+
+
+def bestDistance(charPhoto):
+    hist, _ = np.histogram(charPhoto, bins=256)
+    histToCompare= hist / np.sum(hist)
+
+    topDistance = float('inf')
+    topLetterDigit = None
+
+    pathLetters = "dataset/SameSizeLetters"
+    pathDigits = "dataset/SameSizeNumbers"
+
+    for i in range(1,28):
+        fullPath = None
+        if(i<18):
+
+            fullPath = os.path.join(pathLetters,f'{i}.bmp')
+
+        if(i>=18):
+            temp = i-18
+            fullPath = os.path.join(pathDigits,f'{temp}.bmp')
+
+        img = cv2.imread(fullPath)
+        hist, _ = np.histogram(img, bins=256)
+        hist = hist / np.sum(hist)
+        value = euclideanDistance(hist, histToCompare)
+        if (value < topDistance):
+            topLetterDigit = img
+            topDistance = value
+
+    return topLetterDigit,topDistance
 
 
 
+
+def euclideanDistance(img1, img2):
+    distance = np.sqrt(np.sum((img1 - img2) ** 2))
+
+    return distance
+
+
+#returns an array of Digits/Letters
 def processEachDL(image, epsilon,k1,k2,ratioStandard):
-	#1
+    #1
 
-	#TODO: max digit size, min digit size have to be set, minimum ration
-	#TODO: so 3 things
+    listaChars = []
 
-	#We convert RGB->Gray
-	imgGray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-	#separatin into a binary image
-	image = isodata_thresholding(imgGray,epsilon)
-	# inverting so letter/digits are white the plate is black
-	imageInverted = 255-image
+    #We convert RGB->Gray
+    imgGray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    #separatin into a binary image
+    image = isodata_thresholding(imgGray,epsilon)
+    # inverting so letter/digits are white the plate is black
+    imageInverted = 255-image
 
-	plt.imshow(cv2.cvtColor(imageInverted,cv2.COLOR_GRAY2RGB))
-	plt.title('Green')
-	plt.axis('off')
-	plt.show()
+    # plt.imshow(cv2.cvtColor(imageInverted,cv2.COLOR_GRAY2RGB))
+    # plt.title('Green')
+    # plt.axis('off')
+    # plt.show()
 
-	# Calculate an area of the image so then we can use that to discard not valid contours
-	height, width = image.shape[:2]
-	MAX_AREA = height*width
+    # Calculate an area of the image so then we can use that to discard not valid contours
+    height, width = image.shape[:2]
+    MAX_AREA = height*width
 
-	#Now if we have find countours for each digit
-	contours,_ = cv2.findContours(imageInverted,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	for contour in contours:
-		area = cv2.contourArea(contour)
-		#if this contour is too big we discard it
-		if(MAX_AREA*k1<area or MAX_AREA*k2>area):
-			continue
-
-
-		x, y, w, h = cv2.boundingRect(contour)
+    #Now if we have find countours for each digit
+    contours,_ = cv2.findContours(imageInverted,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        #if this contour is too big we discard it
+        if(MAX_AREA*k1<area or MAX_AREA*k2>area):
+            continue
 
 
-		#ratio
-		roi = imageInverted[y:y + h, x:x + w]
-
-		nonZeroPixelsInTheRectangle = cv2.countNonZero(roi)
-		allPixelsInTheRectangle = w * h
-		ratio = nonZeroPixelsInTheRectangle/allPixelsInTheRectangle
-
-		if(ratio<ratioStandard):
-			continue
+        x, y, w, h = cv2.boundingRect(contour)
 
 
+        #ratio
+        roi = imageInverted[y:y + h, x:x + w]
 
-		imageCropped = imageInverted[y:y + h, x:x + w]
+        nonZeroPixelsInTheRectangle = cv2.countNonZero(roi)
+        allPixelsInTheRectangle = w * h
+        ratio = nonZeroPixelsInTheRectangle/allPixelsInTheRectangle
 
-		# visualization
-		plt.imshow(cv2.cvtColor(imageCropped, cv2.COLOR_GRAY2RGB))
+        if(ratio<ratioStandard):
+            continue
 
-		plt.title('Green')
-		plt.axis('off')
-		plt.show()
 
+
+        imageCropped = imageInverted[y:y + h, x:x + w]
+        listaChars.append(imageCropped)
+
+        # visualization
+        # plt.imshow(cv2.cvtColor(imageCropped, cv2.COLOR_GRAY2RGB))
+		#
+        # plt.title('Green')
+        # plt.axis('off')
+        # plt.show()
+    return listaChars
 
 
 
 def isodata_thresholding(image, epsilon = 2):
-	# Compute the histogram and set up variables
-	hist = np.array(cv2.calcHist([image], [0], None, [256], [0, 256])).flatten()
-	tau = np.random.randint(hist.nonzero()[0][0], 256 - hist[::-1].nonzero()[0][0])
-	old_tau = -2*epsilon
-	# Iterations of the isodata thresholding algorithm
-	while(abs(tau - old_tau) >= epsilon):
-		ForegroundMask = image >= tau
-		#TODO Calculate m1
-		m1 = image[ForegroundMask]
-		#this so there is no division by zero problem
-		m1Len = np.count_nonzero(m1) if np.count_nonzero(m1)!=0 else 0.0001
-		m1Sum = np.sum(m1)
-		m1 = m1Sum/m1Len
-		#TODO Calculate m2
-		BackgroundMask = image < tau
-		m2 = image[BackgroundMask]
-		m2Len = np.count_nonzero(m2) if np.count_nonzero(m2) != 0 else 0.0001
-		m2Sum = np.sum(m2)
-		m2 = m2Sum / m2Len
-		# TODO Calculate new tau
-		old_tau = tau
-		tau = (m1 + m2) / 2
-	# TODO Threshold the image based on last tau
-	# background = np.where(image < tau, image, 0)
-	# foreground = np.where(image >= tau, image, 0)
-	image = np.where(image>=tau,255,0).astype(np.uint8)
-	return image
+    # Compute the histogram and set up variables
+    hist = np.array(cv2.calcHist([image], [0], None, [256], [0, 256])).flatten()
+    tau = np.random.randint(hist.nonzero()[0][0], 256 - hist[::-1].nonzero()[0][0])
+    old_tau = -2*epsilon
+    # Iterations of the isodata thresholding algorithm
+    while(abs(tau - old_tau) >= epsilon):
+        ForegroundMask = image >= tau
+        #TODO Calculate m1
+        m1 = image[ForegroundMask]
+        #this so there is no division by zero problem
+        m1Len = np.count_nonzero(m1) if np.count_nonzero(m1)!=0 else 0.0001
+        m1Sum = np.sum(m1)
+        m1 = m1Sum/m1Len
+        #TODO Calculate m2
+        BackgroundMask = image < tau
+        m2 = image[BackgroundMask]
+        m2Len = np.count_nonzero(m2) if np.count_nonzero(m2) != 0 else 0.0001
+        m2Sum = np.sum(m2)
+        m2 = m2Sum / m2Len
+        # TODO Calculate new tau
+        old_tau = tau
+        tau = (m1 + m2) / 2
+    # TODO Threshold the image based on last tau
+    # background = np.where(image < tau, image, 0)
+    # foreground = np.where(image >= tau, image, 0)
+    image = np.where(image>=tau,255,0).astype(np.uint8)
+    return image
 
 
 plate_image_path = "recognitionTestPlate.png"
